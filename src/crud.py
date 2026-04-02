e90bda6ac4ab2f9e60a812117fa35adb521021e9
LuanBasani <luanbasani@gmail.com>
2026-04-02 11:01:47 -0300

    02/04/26 - Criando arquivo principal

---
+++ src/crud.py
@@ -0,0 +1,11 @@
+import json
+
+
+# Menu principal
+def menu():
+    print("\n--- Mini Steam ---")
+    print("1. Adicionar Jogo")
+    print("2. Ver Biblioteca / Wishlist")
+    print("3. Atualizar Jogo (ex: atualizar horas jogadas)")
+    print("4. Remover Jogo (ex: pedir reembolso)")
+    print("0. Deslogar")
